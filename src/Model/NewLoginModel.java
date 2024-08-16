766e952820f11ac93d71f2480d659508630b9235
Saurav <maharjansaurav2002@gmail.com>
2024-08-16 16:47:05 +0545

    Full code

--- src/Model/NewLoginModel.java
+++ src/Model/NewLoginModel.java
@@ -9,5 +9,17 @@ package Model;
  * @author Saurav
  */
 public class NewLoginModel {
+    private String email;
+    private String password;
     
+    public NewLoginModel(String email,String password){
+        this.email=email;
+        this.password=password;   
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
 }
--- src/Model/RegisterModel.java
+++ src/Model/RegisterModel.java
@@ -9,5 +9,32 @@ package Model;
  * @author Saurav
  */
 public class RegisterModel {
+    private String username;
+    private String email;
+    private String password;
+    private String phone;
+    private String NID;
     
+    public RegisterModel(String username,String email,String password,String phone,String NID){
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.phone=phone;
+        this.NID=NID;
+    }
+    public String getUserName(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public String getNID(){
+        return NID;
+    }
 }
